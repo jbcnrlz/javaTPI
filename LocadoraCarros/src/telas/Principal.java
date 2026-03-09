c82482c8601e075c66e017a66352c67123481f0b
João Cardia <joao.cardia@fatec.sp.gov.br>
2026-03-09 11:52:22 -0300

    Aula 090326

---
+++ LocadoraCarros/src/telas/Principal.java
@@ -0,0 +1,31 @@
+package telas;
+
+import dados.Carro;
+
+public class Principal {
+
+	public static void main(String args[]) {
+		Carro c1 = new Carro("GM", "Onyx", 2017, "Preto");
+		Carro c2 = new Carro("Wolksvagem","UP",2015,"Vermelho");
+		Carro c3 = new Carro("Toyota","Yaris",2017,"Vermelho");
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+		if (c1.equals(c2)) {
+			System.out.println("São iguais!");
+		} else {
+			System.out.println("São diferentes!");
+		}
+		
+		Carro c4 = new Carro("GM", "Onyx", 2017, "Preto");
+		if (c1.equals(c4)) {
+			System.out.println("São iguais!");
+		} else {
+			System.out.println("São diferentes!");
+		}
+		
+	}
+	
+}
