a817b51bb09e11f815d1bc88f4d202f171a7e6e4
João Cardia <joao.cardia@fatec.sp.gov.br>
2026-03-17 09:30:40 -0300

    Aula 170326

--- ContabilSis/src/regrasDeNegocio/FolhaDePagamento.java
+++ ContabilSis/src/regrasDeNegocio/FolhaDePagamento.java
@@ -1,9 +1,33 @@
 package regrasDeNegocio;
 
+import java.util.LinkedList;
+
 public class FolhaDePagamento {
 
-	public String imprimirSalario(Funcionario f) {
-		return f.getNome() + " - " + Double.toString(f.calcularSalario());
+	private LinkedList<Funcionario> funcs;
+	
+	public FolhaDePagamento() {
+		this.funcs = new LinkedList<>();	
+	}
+	
+	public void addFuncionario(Funcionario f) {
+		this.funcs.add(f);
+	}
+	
+	public void removeFuncionario(Funcionario f) {
+		this.funcs.remove(f);
+	}
+	
+	public Funcionario getFuncionario(int idx) {
+		return this.funcs.get(idx);
+	}
+	
+	public String imprimirSalario() {
+		String retorno = "Folha de pagamento\n";
+		for (Funcionario f : this.funcs) {
+			retorno += f.getNome() + " - " + Double.toString(f.calcularSalario()) + "\n";
+		}
+		return retorno;
 	}
 	
 }
