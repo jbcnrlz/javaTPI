52a1609a0723f6a913399269890a1a1461d90045
João Cardia <joao.cardia@fatec.sp.gov.br>
2026-03-23 12:08:47 -0300

    Aula 230326

--- LojaEletronicos\/src/tela/Principal.java
+++ LojaEletronicos\/src/tela/Principal.java
@@ -1,6 +1,7 @@
 package tela;
 
 import dados.Eletronico;
+import dados.Smartphone;
 import dados.Televisao;
 
 public class Principal {
@@ -8,6 +9,11 @@ public class Principal {
 	public static void main(String[] args) {
 		Eletronico el = new Televisao("Samsung",40,"4K",true);
 		System.out.println(el.exibirInformacoes());
+		
+		Eletronico el2 = new Smartphone("Sansumg", "Galaxy S6", 4500, true);
+		System.out.println(el2.exibirInformacoes());
+		el2.ligar();
+		System.out.println(el2.exibirInformacoes());
 	}
 	
 }
